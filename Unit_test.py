7719ddbb6151bebf2cbfcbca485da0beb5d2564e
Gil Shaul-Shay <Gils@mobileye.com>
2020-10-26 19:52:39 +0200

    new update

--- Unit_test.py
+++ Unit_test.py
@@ -1,4 +1,20 @@
-import unittest
 import pytest
 
+
+def capital_case(x):
+    if not isinstance(x, str):
+        raise TypeError('Please provide a string argument')
+        print(TypeError)
+    return x.capitalize()
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        capital_case(9)
+
 print("First")
+test_capital_case()
+test_raises_exception_on_non_string_arguments()
+print("Last")
